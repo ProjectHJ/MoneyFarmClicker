f3f2a059afce4bdf7c42f07f4e2a8a69104407af
Illumi-Nation Studios <illnation.3@gmail.com>
2017-01-25 11:06:40 -0600

    Added Miner

--- app/src/main/java/ill/moneyclicker/com/moneyclicker/MainActivity.java
+++ app/src/main/java/ill/moneyclicker/com/moneyclicker/MainActivity.java
@@ -10,15 +10,12 @@ import android.widget.TextView;
 
 import com.kosalgeek.android.caching.FileCacher;
 
-import org.w3c.dom.Text;
-
 import java.io.IOException;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     double thisMoney;
-    double totalMoney;
     int thisMoneyCounter;
 
     public double treeMoney;
@@ -29,19 +26,24 @@ public class MainActivity extends AppCompatActivity {
     public double copierCost = 150.00;
     public int copierCounter;
 
+    public double minerMoney;
+    public double minerCost = 500.00;
+    public int minerCounter;
+
     Button tMoney;
     Button cMoney;
+    Button mMoney;
     Button makeMoney;
     Chronometer save;
     Chronometer tree;
     Chronometer copier;
+    Chronometer miner;
     EditText thisMoneyText;
-    EditText totalMoneyText;
     TextView moneyTreePriceText;
     TextView moneyCopierPriceText;
+    TextView moneyMinerPriceText;
 
     final FileCacher<Double> thisMoneyCache = new FileCacher<>(MainActivity.this, "Cache_01.cache");
-    final FileCacher<Double> totalMoneyCache = new FileCacher<>(MainActivity.this, "Cache_02.cache");
 
     final FileCacher<Double> treeMoneyCache = new FileCacher<>(MainActivity.this, "Cache_03.cache");
     final FileCacher<Integer> treeCounterCache = new FileCacher<>(MainActivity.this, "Cache_04.cache");
@@ -51,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
     final FileCacher<Double> copierPriceCache = new FileCacher<>(MainActivity.this, "Cache_07.cache");
     final FileCacher<Integer> copierCounterCache = new FileCacher<>(MainActivity.this, "Cache_08.cache");
 
+    final FileCacher<Double> minerMoneyCache = new FileCacher<>(MainActivity.this, "Cache_09.cache");
+    final FileCacher<Double> minerPriceCache = new FileCacher<>(MainActivity.this, "Cache_10.cache");
+    final FileCacher<Integer> minerCounterCache = new FileCacher<>(MainActivity.this, "Cache_11.cache");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,28 +64,31 @@ public class MainActivity extends AppCompatActivity {
 
         tMoney = (Button) findViewById(R.id.moneyTree);
         cMoney = (Button) findViewById(R.id.moneyCopier);
+        mMoney = (Button) findViewById(R.id.moneyMiner);
         makeMoney = (Button) findViewById(R.id.makeMoney);
 
         thisMoneyCounter = 1;
         tree = (Chronometer) findViewById(R.id.tree);
         save = (Chronometer) findViewById(R.id.chronometer);
         copier = (Chronometer) findViewById(R.id.copier);
+        miner = (Chronometer) findViewById(R.id.miner);
         thisMoneyText = (EditText) findViewById(R.id.thisMoney);
-        totalMoneyText = (EditText) findViewById(R.id.totalMoney);
         moneyTreePriceText = (TextView) findViewById(R.id.moneyTreePrice);
         moneyCopierPriceText = (TextView) findViewById(R.id.moneyCopierPrice);
+        moneyMinerPriceText = (TextView) findViewById(R.id.moneyMinerPrice);
         NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
-        thisMoneyText.setText(String.valueOf(currencyFormatter.format(totalMoney)));
 
         try {
             thisMoney = thisMoneyCache.readCache();
-            totalMoney = totalMoneyCache.readCache();
             treeMoney = treeMoneyCache.readCache();
             treeCounter = treeCounterCache.readCache();
             treeCost = treePriceCache.readCache();
             copierMoney = copierMoneyCache.readCache();
             copierCounter = copierCounterCache.readCache();
             copierCost = copierPriceCache.readCache();
+            minerMoney = copierMoneyCache.readCache();
+            minerCounter = copierCounterCache.readCache();
+            minerCost = copierPriceCache.readCache();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -88,23 +97,47 @@ public class MainActivity extends AppCompatActivity {
 
         if (treeCounter > 0) {
             tree.start();
-            tMoney.setText("Money Tree    Level: " + treeCounter);
+            tMoney.setText("Tree Level: " + treeCounter);
             moneyTreePriceText.setText(String.valueOf(currencyFormatter.format(treeCost)));
         }
 
         if (copierCounter > 0) {
             copier.start();
-            cMoney.setText("Money Copier    Level: " + copierCounter);
+            cMoney.setText("Copier Level: " + copierCounter);
             moneyCopierPriceText.setText(String.valueOf(currencyFormatter.format(copierCost)));
+        }
+
+        if (minerCounter > 0) {
+            miner.start();
+            mMoney.setText("Miner Level: " + minerCounter);
+            moneyMinerPriceText.setText(String.valueOf(currencyFormatter.format(minerCost)));
         }
 
         makeMoney.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                thisMoney += ((Math.PI * Math.random()) + ((thisMoney) / 17) * thisMoneyCounter) / 20d;
+                thisMoney += ((Math.PI * Math.random()) + ((thisMoney) / 4) * thisMoneyCounter) / 20d;
 
-                totalMoney += thisMoney;
+                if (treeCost <= thisMoney) {
+                    tMoney.setEnabled(true);
+                } else {
+                    tMoney.setEnabled(false);
+                }
+
+                if (copierCost <= thisMoney) {
+                    cMoney.setEnabled(true);
+                } else {
+                    cMoney.setEnabled(false);
+                }
+
+                if (minerCost <= thisMoney) {
+                    mMoney.setEnabled(true);
+                } else {
+                    mMoney.setEnabled(false);
+                }
+
                 NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
                 thisMoneyText.setText(String.valueOf(currencyFormatter.format(thisMoney)));
+                save.callOnClick();
             }
         });
 
@@ -112,9 +145,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (treeCost <= thisMoney) {
-                    thisMoney -= treeCost;
                     NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
-                    thisMoneyText.setText(String.valueOf(currencyFormatter.format(thisMoney)));
+
+                    thisMoney -= treeCost;
 
                     treeCost += (((Math.PI * Math.random()) * (treeCost * 2))) / 12d;
                     treeCost = treeCost;
@@ -130,7 +163,10 @@ public class MainActivity extends AppCompatActivity {
 
                     treeCounter++;
 
-                    tMoney.setText("Money Tree    Level: " + treeCounter);
+                    save.callOnClick();
+                    tMoney.setText("Tree Level: " + treeCounter);
+
+                    thisMoneyText.setText(String.valueOf(currencyFormatter.format(thisMoney)));
                 }
             }
         });
@@ -141,7 +177,6 @@ public class MainActivity extends AppCompatActivity {
                 if (copierCost <= thisMoney) {
                     thisMoney -= copierCost;
                     NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
-                    thisMoneyText.setText(String.valueOf(currencyFormatter.format(thisMoney)));
 
                     copierCost += (((Math.PI * Math.random()) * (copierCost * 2))) / 12d;
                     copierCost = copierCost;
@@ -157,7 +192,40 @@ public class MainActivity extends AppCompatActivity {
 
                     copierCounter++;
 
-                    cMoney.setText("Money Copier    Level: " + copierCounter);
+                    save.callOnClick();
+                    cMoney.setText("Copier Level: " + copierCounter);
+
+                    thisMoneyText.setText(String.valueOf(currencyFormatter.format(thisMoney)));
+                }
+            }
+        });
+
+        mMoney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (minerCost <= thisMoney) {
+                    NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+
+                    thisMoney -= minerCost;
+
+                    minerCost += (((Math.PI * Math.random()) * (minerCost * 4))) / 12d;
+                    minerCost = minerCost;
+                    moneyMinerPriceText.setText(String.valueOf(currencyFormatter.format(minerCost)));
+
+                    miner.start();
+
+                    if (minerCost <= thisMoney) {
+                        mMoney.setEnabled(true);
+                    } else {
+                        mMoney.setEnabled(false);
+                    }
+
+                    minerCounter++;
+
+                    save.callOnClick();
+                    mMoney.setText("Miner Level: " + minerCounter);
+
+                    thisMoneyText.setText(String.valueOf(currencyFormatter.format(thisMoney)));
                 }
             }
         });
@@ -166,18 +234,6 @@ public class MainActivity extends AppCompatActivity {
         save.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
-                try {
-                    thisMoneyCache.writeCache(thisMoney);
-                    totalMoneyCache.writeCache(totalMoney);
-                    treeMoneyCache.writeCache(treeMoney);
-                    treeCounterCache.writeCache(treeCounter);
-                    treePriceCache.writeCache(treeCost);
-                    copierPriceCache.writeCache(copierCost);
-                    copierMoneyCache.writeCache(copierMoney);
-                    copierCounterCache.writeCache(copierCounter);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
 
                 if (treeCost <= thisMoney) {
                     tMoney.setEnabled(true);
@@ -191,34 +247,49 @@ public class MainActivity extends AppCompatActivity {
                     cMoney.setEnabled(false);
                 }
 
-                totalMoney = thisMoney;
-                NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
-                totalMoneyText.setText(String.valueOf(currencyFormatter.format(totalMoney)));
+                if (minerCost <= thisMoney) {
+                    mMoney.setEnabled(true);
+                } else {
+                    mMoney.setEnabled(false);
+                }
+
+                try {
+                    thisMoneyCache.writeCache(thisMoney);
+                    treeMoneyCache.writeCache(treeMoney);
+                    treeCounterCache.writeCache(treeCounter);
+                    treePriceCache.writeCache(treeCost);
+
+                    copierPriceCache.writeCache(copierCost);
+                    copierMoneyCache.writeCache(copierMoney);
+                    copierCounterCache.writeCache(copierCounter);
+
+                    minerPriceCache.writeCache(minerCost);
+                    minerMoneyCache.writeCache(minerMoney);
+                    minerCounterCache.writeCache(minerCounter);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
         tree.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
+                treeMoney += (((Math.PI * Math.random()) + ((treeMoney / 2) * treeCounter))) / 560d;
+
+                thisMoney += treeMoney;
+
                 if (treeCost <= thisMoney) {
                     tMoney.setEnabled(true);
                 } else {
                     tMoney.setEnabled(false);
                 }
 
-                treeMoney += (((Math.PI * Math.random()) * treeCounter)) / 560d;
-
-                thisMoney += treeMoney;
-
                 try {
                     thisMoneyCache.writeCache(thisMoney);
-                    totalMoneyCache.writeCache(totalMoney);
                     treeMoneyCache.writeCache(treeMoney);
                     treeCounterCache.writeCache(treeCounter);
                     treePriceCache.writeCache(treeCost);
-                    copierPriceCache.writeCache(copierCost);
-                    copierMoneyCache.writeCache(copierMoney);
-                    copierCounterCache.writeCache(copierCounter);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -232,18 +303,10 @@ public class MainActivity extends AppCompatActivity {
         copier.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
-                try {
-                    thisMoneyCache.writeCache(thisMoney);
-                    totalMoneyCache.writeCache(totalMoney);
-                    treeMoneyCache.writeCache(treeMoney);
-                    treeCounterCache.writeCache(treeCounter);
-                    treePriceCache.writeCache(treeCost);
-                    copierPriceCache.writeCache(copierCost);
-                    copierMoneyCache.writeCache(copierMoney);
-                    copierCounterCache.writeCache(copierCounter);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+
+                copierMoney += (((Math.PI * Math.random()) + ((copierMoney / 2) * copierCounter))) / 560d;
+
+                thisMoney += copierMoney;
 
                 if (copierCost <= thisMoney) {
                     cMoney.setEnabled(true);
@@ -251,9 +314,46 @@ public class MainActivity extends AppCompatActivity {
                     cMoney.setEnabled(false);
                 }
 
-                totalMoney = thisMoney;
+                try {
+                    thisMoneyCache.writeCache(thisMoney);
+                    copierPriceCache.writeCache(copierCost);
+                    copierMoneyCache.writeCache(copierMoney);
+                    copierCounterCache.writeCache(copierCounter);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
-                totalMoneyText.setText(String.valueOf(currencyFormatter.format(totalMoney)));
+                thisMoneyText.setText(String.valueOf(currencyFormatter.format(thisMoney)));
+            }
+        });
+
+        miner.getOnChronometerTickListener();
+        miner.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+
+                minerMoney += (((Math.PI * Math.random()) + ((minerMoney / 2) * minerCounter))) / 560d;
+
+                thisMoney += minerMoney;
+
+                if (minerCost <= thisMoney) {
+                    mMoney.setEnabled(true);
+                } else {
+                    mMoney.setEnabled(false);
+                }
+
+                try {
+                    thisMoneyCache.writeCache(thisMoney);
+                    minerPriceCache.writeCache(minerCost);
+                    minerMoneyCache.writeCache(minerMoney);
+                    minerCounterCache.writeCache(minerCounter);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+                thisMoneyText.setText(String.valueOf(currencyFormatter.format(thisMoney)));
             }
         });
     }
